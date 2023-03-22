2a7c5ed37bf724f1b7cc956ef8e22e66ae4544bf
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-03-22 08:21:38 -0300

    primeiras mudançãs  com o pycharm

--- deafio012.py
+++ deafio012.py
@@ -1,2 +1,3 @@
+import math
 preco = float(input('Digite o preço do produto: R$'))
 print(f'O produto na promoção com desconto de 5%, de R${preco :.2f}, saíra por R${preco * 0.95 :.2f}')
---
+++ desafio016.py
@@ -0,0 +1,6 @@
+num = float(input('Digite um numero real: '))
+print(f'O valor digitado foi {num}, e sua parte inteira é {int(num)}.')
+
+# from math import trunc
+# num = float(input("Digite um valor: "))
+# print('O valor digitado foi {}, e sua parte inteira é {}. '.format(num, trunc((num))))
