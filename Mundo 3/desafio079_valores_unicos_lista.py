946ccd1fb269b52a7eed10eb3252d035bc7b9a16
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-04-09 15:25:35 -0300

    desafio 079

--- Mundo 3/desafio079_valores_unicos_lista.py
+++ Mundo 3/desafio079_valores_unicos_lista.py
@@ -3,10 +3,10 @@ while True:
     valor = int(input('Digite um valor: '))
     if valor not in lista:
         lista.append(valor)
-        print('Valor add com sucesso!)
+        print('Valor add com sucesso!')
     else:
         print('Valor duplicado, tente outro: ')
-    r = str(input('Quer continuar? [S/N]: ')
+    r = str(input('Quer continuar? [S/N]: '))
     if r in 'Nn':
         break
 print(lista)
