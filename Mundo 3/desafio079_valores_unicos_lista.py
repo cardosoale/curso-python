b29ee621fdbfb60864d9f62b9f35591f98bb5b57
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-04-09 15:32:10 -0300

    acertos 079

--- Mundo 3/desafio079_valores_unicos_lista.py
+++ Mundo 3/desafio079_valores_unicos_lista.py
@@ -5,8 +5,8 @@ while True:
         lista.append(valor)
         print('Valor add com sucesso!')
     else:
-        print('Valor duplicado, tente outro: ')
-    r = str(input('Quer continuar? [S/N]: '))
+        print('Valor duplicado, não será inserido!')
+    r = str(input('Para SAIR digite "N", ou qualquer outro caractere, para continuar: '))
     if r in 'Nn':
         break
-print(lista)
+print(f'Vc digitou os valores: {lista}')
